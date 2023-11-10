2ecc695fd2f30d7e39d6ed950bbb5bcac20e9792
gedeonagmas <103260131+gedeonagmas@users.noreply.github.com>
2023-11-10 11:18:48 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ process.on("uncaughtException", (err) => {
 });
 
 app.use(express.json());
-// app.use("/hotel/app/v1", router);
+app.use("/hotel/app/v1", router);
 
 app.all("*", (req, res, next) => {
   res.status(200).json({ message: `${req.originalUrl} is invalid url` });
@@ -30,9 +30,9 @@ app.all("*", (req, res, next) => {
 
 app.use(errorController);
 
-app.get("/", (req, res, next) => {
-  res.send("hello well come to my hotel app");
-});
+// app.get("/", (req, res, next) => {
+//   res.send("hello well come to my hotel app");
+// });
 mongodb()
   .then(() => {
     app.listen(process.env.PORT, (err) => {
