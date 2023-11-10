513699824ec0d0d0682e103265685feb33b0686a
gedeonagmas <103260131+gedeonagmas@users.noreply.github.com>
2023-11-10 11:10:09 +0300

    Update apiSlice.js

--- client/src/features/api/apiSlice.js
+++ client/src/features/api/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/hotel/app/v1",
+    baseUrl: "https://hotel-app-server.vercel.app/hotel/app/v1",
   }),
   tagTypes: [
     "profile-info",
